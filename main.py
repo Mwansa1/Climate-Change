5902662c0ad477f7534b75ff2798b06f701a3dac
Mwansa Puta <mwansa.puta@my.metrostate.edu>
2021-07-29 20:23:21 +0000

    added basic home.html and css
    Co-authored-by: Melanie Jimenez <mmjimenez@owu.edu>
    Co-authored-by: Cristina de Jong <cdejong2@jhu.edu>
    Co-authored-by: Melisa Calderon <mecalderon@vassar.edu>

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect,request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from turbo_flask import Turbo
+import requests
+from weatherAPI import search
+# from weatherAPI import 
 # import statements from prev projects, add/remove as needed
 
 app = Flask(__name__)
@@ -10,15 +13,26 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html',
-                           subtitle='Home Page',
-                           text='Welcome to Climate Change project!')
+    return render_template('home.html')
 # add more pages as needed
-# @app.route("/login")
-# def login():
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/search_by_city", methods=["POST"])
+def search_by_city():
+    city = request.form["city"]
+    data = search(city)
+#     print(data)
+    return render_template("home.html", data=data) 
+
+  
+
     
-# @app.route("/more")
-# def second_page():
     
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
