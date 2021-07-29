97864a2a1ee7869c5b20eab75cd212768cc183ac
Tina de Jong <cdejong2@jhu.edu>
2021-07-29 20:52:44 +0000

    register page working

--- main.py
+++ main.py
@@ -3,24 +3,48 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from turbo_flask import Turbo
 from forms import RegistrationForm
+from sqlalchemy import exc, text
 # import statements from prev projects, add/remove as needed
 
 app = Flask(__name__)
 turbo = Turbo(app)
+bcrypt = Bcrypt(app)
 
 app.config['SECRET_KEY'] = '525901fece4e62b2eb11fa3c1a302835'
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(20), unique=True, nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
-  password = db.Column(db.String(60), nullable=False)
+    """An admin user capable of viewing reports.
+    :param str email: email address of user
+    :param str password: encrypted password for the user
+    """
+    __tablename__ = 'user'
 
-  def __repr__(self):
-    return f"User('{self.username}', '{self.email}')"
+    username = db.Column(db.String(20), primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, anonymous users aren't supported."""
+        return False
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
 
 # basic homepage, to be edited as needed with layout.html and main.css
 @app.route("/")
@@ -30,13 +54,13 @@ def home():
                            subtitle='Home Page',
                            text='Welcome to Climate Change project!')
 # add more pages as needed
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         password = (bcrypt.generate_password_hash(form.password.data)
                     .decode('utf-8'))
-        user = User(email=form.email.data,
+        user = User(username=form.username.data,email=form.email.data,
                     password=password)
         try:
             db.session.add(user)
@@ -45,7 +69,7 @@ def register():
             db.session.rollback()
             flash(f'Username or email account already exists!', 'success')
         else:
-            flash(f'Account created for {form.email.data}!', 'success')
+            flash(f'Account created for {form.username.data}!', 'success')
             return redirect(url_for('home'))  # if so - send to home page
     return render_template('register.html', title='Register', form=form)
 
