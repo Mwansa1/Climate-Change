0bd2656dbdac3cf02952e2749bff748e9f3c1b42
Tina de Jong <cdejong2@jhu.edu>
2021-07-29 16:32:10 +0000

    working on login forms

--- main.py
+++ main.py
@@ -2,9 +2,25 @@ from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from turbo_flask import Turbo
+from forms import RegistrationForm
 # import statements from prev projects, add/remove as needed
 
 app = Flask(__name__)
+turbo = Turbo(app)
+
+app.config['SECRET_KEY'] = '525901fece4e62b2eb11fa3c1a302835'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
 
 # basic homepage, to be edited as needed with layout.html and main.css
 @app.route("/")
@@ -14,9 +30,25 @@ def home():
                            subtitle='Home Page',
                            text='Welcome to Climate Change project!')
 # add more pages as needed
-# @app.route("/login")
-# def login():
-    
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        password = (bcrypt.generate_password_hash(form.password.data)
+                    .decode('utf-8'))
+        user = User(email=form.email.data,
+                    password=password)
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except exc.IntegrityError:
+            db.session.rollback()
+            flash(f'Username or email account already exists!', 'success')
+        else:
+            flash(f'Account created for {form.email.data}!', 'success')
+            return redirect(url_for('home'))  # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
+
 # @app.route("/more")
 # def second_page():
     
