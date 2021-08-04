be1f99fc47f89ebe3dc417dc6525fd6f5a7329af
Melisa Calderon <mecalderon@vassar.edu>
2021-08-04 16:35:06 +0000

    fixed button

--- blog.py
+++ blog.py
@@ -18,14 +18,20 @@ from wtforms.validators import (
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms.widgets import TextArea
 
+
 class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()] )
-    content = StringField('Post', validators=[DataRequired()], widget=TextArea())
+    title = StringField('Title', validators=[DataRequired()])
+    content = StringField(
+        'Post',
+        validators=[
+            DataRequired()],
+        widget=TextArea())
     submit = SubmitField('Post')
 
 
 class UploadForm(FlaskForm):
-    image = FileField('Upload Image', validators=[DataRequired(),
-        FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+    image = FileField('Upload Image', validators=[
+                      DataRequired(), FileAllowed(['jpg', 'png', 'jpeg'],
+                                                  'Images only!')])
     caption = StringField('caption')
     submit = SubmitField('Upload to Feed')
