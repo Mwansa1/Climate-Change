eefa3a355f5371e05244b4f5f0a5773cba749120
Tina de Jong <cdejong2@jhu.edu>
2021-08-04 18:43:01 +0000

    suggestions search and found page working, mylist not working yet

---
+++ suggestions.py
@@ -0,0 +1,24 @@
+# food suggestions
+food_suggestions = [
+        {'id': 0, 'content': 'Avoid overly processed foods. These foods have a high carbon footprint due to manufacuring, traveling, and distribution.'},
+        {'id': 1, 'content': 'Buy locally made food, your carbon footprint will decrease as a result.'},
+        {'id': 2, 'content': 'Eat less red meat to reduce your carbon footprint.'},
+        {'id': 3, 'content': 'Buy fair trade products to reduce your carbon footprint.'},
+        {'id': 4, 'content': 'Grow your own food to save natural resources in mass production.'}
+    ]
+    
+travel_suggestions = [
+        {'id': 0, 'content': 'Ride a bike or walk to places that are close by instead of driving.'},
+        {'id': 1, 'content': 'Use public transportation instead of driving.'},
+        {'id': 2, 'content': 'Mitigate the negative impact of air travel by flying less often.'},
+        {'id': 3, 'content': 'Don’t speed! Gas mileage declines rapidly above 60 mph. Each 5 mph increase above 60 is like paying an additional 10 cents a gallon for gasoline.'},
+        {'id': 4, 'content': 'Drive hybrid or electric vehicles to reduce pollution from exhaust.'}
+    ]
+    
+energy_suggestions = [
+        {'id': 0, 'content': 'Unplug electronics you are not using to save energy.'},
+        {'id': 1, 'content': 'Replace your light bulb with LED ones, they save 75% more energy!'},
+        {'id': 2, 'content': 'Turn off the lights in rooms you are not in.'},
+        {'id': 3, 'content': 'Avoid turning on the light when you can use sunlight instead.'},
+        {'id': 4, 'content': 'Use dimmers in common areas of your house to save a significant amount of energy'}
+    ]
