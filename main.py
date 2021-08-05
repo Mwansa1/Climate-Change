e7b245d4047757f89130c78673079f2936962c87
Mwansa Puta <mwansa.puta@my.metrostate.edu>
2021-08-05 23:58:41 +0000

    heroku update

--- main.py
+++ main.py
@@ -431,7 +431,7 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, UPLOAD_FOLDER, picture_fn)
-    output_size = (125, 125)
+    output_size = (500, 500)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
