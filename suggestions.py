c4b82ca324cad4d8e4453574c4373f3c8b7f7da4
Melisa Calderon <mecalderon@vassar.edu>
2021-08-05 02:24:04 +0000

    style change

--- suggestions.py
+++ suggestions.py
@@ -1,27 +1,45 @@
 # food suggestions
 food_suggestions = [
-        {'id': 0, 'content': 'Avoid overly processed foods. These foods have a high carbon footprint due to manufacuring, traveling, and distribution.'},
-        {'id': 1, 'content': 'Buy locally made food, your carbon footprint will decrease as a result.'},
-        {'id': 2, 'content': 'Eat less red meat to reduce your carbon footprint.'},
-        {'id': 3, 'content': 'Buy fair trade products to reduce your carbon footprint.'},
-        {'id': 4, 'content': 'Grow your own food to save natural resources in mass production.'},
-        {'id': 5, 'content': 'Use fruit and vegetable scraps as compost instead of throwing them away'}
+        {'id': 0, 'content': 'Avoid overly processed foods. These foods '
+         + 'have a high carbon footprint due to manufacuring, traveling, '
+         + 'and distribution.'},
+        {'id': 1, 'content': 'Buy locally made food, your carbon footprint '
+         + 'will decrease as a result.'},
+        {'id': 2, 'content': 'Eat less red meat to reduce your carbon '
+         + 'footprint.'},
+        {'id': 3, 'content': 'Buy fair trade products to reduce your carbon'
+         + ' footprint.'},
+        {'id': 4, 'content': 'Grow your own food to save natural resources in'
+         + ' mass production.'},
+        {'id': 5, 'content': 'Use fruit and vegetable scraps as compost '
+         + 'instead of throwing them away'}
     ]
 
 travel_suggestions = [
-        {'id': 0, 'content': 'Ride a bike or walk to places that are close by instead of driving.'},
+        {'id': 0, 'content': 'Ride a bike or walk to places that are close by'
+         + ' instead of driving.'},
         {'id': 1, 'content': 'Use public transportation instead of driving.'},
-        {'id': 2, 'content': 'Mitigate the negative impact of air travel by flying less often.'},
-        {'id': 3, 'content': 'Don’t speed! Gas mileage declines rapidly above 60 mph. Each 5 mph increase above 60 is like paying an additional 10 cents a gallon for gasoline.'},
-        {'id': 4, 'content': 'Drive hybrid or electric vehicles to reduce pollution from exhaust.'},
-        {'id': 5, 'content': 'Carpool to save on gas and to help save the environment!'}
+        {'id': 2, 'content': 'Mitigate the negative impact of air travel by '
+         + 'flying less often.'},
+        {'id': 3, 'content': 'Don’t speed! Gas mileage declines rapidly above'
+         + ' 60 mph. Each 5 mph increase above 60 is like paying an additional'
+         + ' 10 cents a gallon for gasoline.'},
+        {'id': 4, 'content': 'Drive hybrid or electric vehicles to reduce '
+         + 'pollution from exhaust.'},
+        {'id': 5, 'content': 'Carpool to save on gas and to help save the '
+         + 'environment!'}
     ]
 
 energy_suggestions = [
-        {'id': 0, 'content': 'Unplug electronics you are not using to save energy.'},
-        {'id': 1, 'content': 'Replace your light bulb with LED ones, they save 75% more energy!'},
+        {'id': 0, 'content': 'Unplug electronics you are not using to save '
+         + 'energy.'},
+        {'id': 1, 'content': 'Replace your light bulb with LED ones, they save'
+         + ' 75% more energy!'},
         {'id': 2, 'content': 'Turn off the lights in rooms you are not in.'},
-        {'id': 3, 'content': 'Avoid turning on the light when you can use sunlight instead.'},
-        {'id': 4, 'content': 'Use dimmers in common areas of your house to save a significant amount of energy'},
-        {'id': 5, 'content': 'Make use of natural lighting to save on energy costs.'}
+        {'id': 3, 'content': 'Avoid turning on the light when you can use '
+         + 'sunlight instead.'},
+        {'id': 4, 'content': 'Use dimmers in common areas of your house to '
+         + 'save a significant amount of energy'},
+        {'id': 5, 'content': 'Make use of natural lighting to save on energy '
+         + 'costs.'}
     ]
