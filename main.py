ecc807461f623833196453123785d70a9461522a
Melisa Calderon <mecalderon@vassar.edu>
2021-08-05 23:05:32 +0000

    final touch

--- main.py
+++ main.py
@@ -265,7 +265,9 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
-
+@app.route('/team')
+def team():
+    return render_template('team.html')
 
 @app.route("/suggestions", methods=['GET', 'POST'])
 def suggestions_search():
